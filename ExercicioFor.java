c3bd47d297c9dc149f27323c4d9debcfc5c58a72
Vivian Kelen <viviankeleng@gmail.com>
2025-07-10 11:05:11 -0300

    Add files via upload

---
+++ ExercicioFor.java
@@ -0,0 +1,16 @@
+public class ExercicioFor {
+    public static void main(String[] args) {
+        //Imprima todos os números pares de 0 a 1000000
+
+       for (int i = 0; i <= 1000000; i++){
+           if(i % 2== 0){
+               System.out.println(i);
+           }
+            ;
+
+
+
+        }
+
+    }
+}
